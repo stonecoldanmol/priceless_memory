a9426c3fec3fe5ca306fe10b5ed88014afc0e024
Anmol Sinha <sinhaanmol.16@gmail.com>
2020-06-28 23:49:56 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/pricelessmemory/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pricelessmemory
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/tile_model.dart
@@ -0,0 +1,25 @@
+class TileModel{
+
+  String imageAssetPath;
+  bool isSelected;
+
+  TileModel({this.imageAssetPath,this.isSelected});
+
+  void setImageAssetPath(String getImageAssetPath){
+    imageAssetPath = getImageAssetPath;
+  }
+
+  void setIsSelected(bool getIsSelected){
+    isSelected=getIsSelected;
+  }
+
+  String getImageAssetPath(){
+    return imageAssetPath;
+  }
+
+  bool getIsSelected(){
+    return isSelected;
+  }
+
+
+}
